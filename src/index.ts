eb05013d040dd3f15ac85f8ef76c9509325d10dc
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-08 22:19:14 -0400

    feat: Use API Route

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
+const router = express.Router()
 
 const PORT = process.env.PORT || 3000
+const API = process.env.API || '/api/v1'
 
-app.get('/', (_, res) => {
+router.get('/', (_, res) => {
   res.send('Hello World!')
 })
+
+router.get('/users', (_, res) => {
+  res.json([{ id: 1, name: 'Alice' }])
+})
+
+app.use(API, router)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
