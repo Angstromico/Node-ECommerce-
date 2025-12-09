33bb6aebf5074a2c68c21defe0c578119958f412
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-08 22:05:21 -0400

    feat: Set Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, TypeScript!')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (_, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
